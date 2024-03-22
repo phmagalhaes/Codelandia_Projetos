60e62bdb1d8efdefab9bc7d329af36385f44443c
Pedro Magalhães <142751028+phmagalhaes@users.noreply.github.com>
2024-03-22 16:14:21 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const page = document.getElementById("pages")
+function pageRedirect(){
+    if(page.value === "Desafio 1"){
+        window.open("https://phmagalhaes.github.io/Codelandia_Projetos/Desafio01/index.html", "_blank")
+    }
+    if(page.value === "Desafio 2"){
+        window.open("https://phmagalhaes.github.io/Codelandia_Projetos/Desafio02/index.html", "_blank")
+    }
+    if(page.value === "Desafio 3"){
+        window.open("https://phmagalhaes.github.io/Codelandia_Projetos/Desafio03/index.html", "_blank")
+    }
+    if(page.value === "Desafio 4"){
+        window.open("https://phmagalhaes.github.io/Codelandia_Projetos/Desafio04/index.html", "_blank")
+    }
+    if(page.value === "Desafio 5"){
+        window.open("https://phmagalhaes.github.io/Codelandia_Projetos/Desafio05/index.html", "_blank")
+    }
+    if(page.value === "Desafio 6"){
+        window.open("https://phmagalhaes.github.io/Codelandia_Projetos/Desafio06/index.html", "_blank")
+    }
+    if(page.value === "Desafio 8"){
+        window.open("https://phmagalhaes.github.io/Codelandia_Projetos/Desafio08/index.html", "_blank")
+    }
+    if(page.value === "Desafio 9"){
+        window.open("https://phmagalhaes.github.io/Codelandia_Projetos/Desafio09/index.html", "_blank")
+    }
+}
